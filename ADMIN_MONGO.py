97a1cbec4206bbe6a0808010620b433215e8943b
BryanJCruz <145821012+BryanJCruz@users.noreply.github.com>
2023-11-13 15:08:32 -0500

    Add files via upload
    actualizacion

--- ADMIN_MONGO.py
+++ ADMIN_MONGO.py
@@ -93,6 +93,42 @@ def eliminar_producto(producto_id):
         print(f"Producto con ID {producto_id} eliminado con éxito.")
     else:
         print(f"No se encontró un producto con ID {producto_id}.")
+        
+
+def buscar_por_id(coleccion, objeto_id):
+    # Buscar el objeto por ID
+    objeto = coleccion.find_one({"_id": objeto_id})
+    
+    if objeto:
+        # Mostrar los detalles del objeto encontrado
+        print(f"Objeto encontrado:\n{objeto}")
+    else:
+        print(f"No se encontró un objeto con ID {objeto_id} en la colección.")
+
+def actualizar_por_id(coleccion, objeto_id):
+    # Buscar el objeto por ID
+    objeto = coleccion.find_one({"_id": objeto_id})
+    
+    if objeto:
+        # Mostrar los detalles actuales del objeto
+        print(f"Detalles actuales del objeto con ID {objeto_id}:\n{objeto}")
+        
+        # Solicitar los nuevos datos al usuario
+        nuevos_datos = {}
+        for clave, valor_actual in objeto.items():
+            if clave != "_id":
+                nuevo_valor = input(f"Ingrese el nuevo valor para {clave} ({valor_actual}): ")
+                nuevos_datos[clave] = nuevo_valor if nuevo_valor else valor_actual
+        
+        # Actualizar el objeto con los nuevos datos
+        result = coleccion.update_one({"_id": objeto_id}, {"$set": nuevos_datos})
+        
+        if result.modified_count > 0:
+            print(f"Objeto con ID {objeto_id} actualizado con éxito.")
+        else:
+            print(f"No se pudo actualizar el objeto con ID {objeto_id}.")
+    else:
+        print(f"No se encontró un objeto con ID {objeto_id} en la colección.")
 
 
 #MENU
@@ -104,7 +140,6 @@ while True:
     print("4. Actualizar")
     print("5. Salir")
     print("0. Crear Base de Datos")
-    print("999. PANICO")
 
     try:
         opcion = int(input("\nOpcion: "))
@@ -169,13 +204,42 @@ while True:
                 time.sleep(2)
                 break
     elif opcion == 3:
-        pass
+            print("\n\t\t Buscar")
+            print("1. Buscar por ID en Proveedores")
+            print("2. Buscar por ID en Productos")
+            print("3. Buscar por ID en Distribuidores")
+            try:
+                op = int(input("\nOpcion: "))
+            except:
+                op = 0
+            if op == 1:
+                id = int(input("Ingrese la ID del Proveedor: "))
+                buscar_por_id(proveedores, id)
+            elif op == 2:
+                id = int(input("Ingrese la ID del Producto: "))
+                buscar_por_id(productos, id)
+            elif op == 3:
+                id = int(input("Ingrese la ID del Distribuidor: "))
+                buscar_por_id(distribuidores, id)
     elif opcion == 4:
-        pass
+            print("\n\t\t Actualizar")
+            print("1. Actualizar datos de Proveedor por ID")
+            print("2. Actualizar datos de Producto por ID")
+            print("3. Actualizar datos de Distribuidor por ID")
+            try:
+                op = int(input("\nOpcion: "))
+            except:
+                op = 0
+            if op == 1:
+                id = int(input("Ingrese la ID del Proveedor: "))
+                actualizar_por_id(proveedores, id)
+            elif op == 2:
+                id = int(input("Ingrese la ID del Producto: "))
+                actualizar_por_id(productos, id)
+            elif op == 3:
+                id = int(input("Ingrese la ID del Distribuidor: "))
+                actualizar_por_id(distribuidores, id)
     elif opcion == 5:
         break
     elif opcion == 0:
         pass
-    elif opcion == 999:
-        print("PANICO")
-
